a6d6e6493c3ad42613d35a5ccda89df4c9f7521d
dibx <debyendu@alumnux.com>
2019-09-08 19:12:23 +0530

    removed extra styles and addd directive component

--- AngularDirectives/src/app/app.module.ts
+++ AngularDirectives/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestDirectivesComponent } from './test-directives/test-directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestDirectivesComponent
   ],
   imports: [
     BrowserModule
---
+++ AngularDirectives/src/app/test-directives/test-directives.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: '[app-ng-directives]',
+    template: `
+
+    `,
+    styles: []
+})
+export class TestDirectivesComponent implements OnInit {
+
+    public title="Angular directives";
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
